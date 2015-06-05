c252e9236a935f7ecfe1caf61df5283625f79cce
hosopy <dev.hosopy@gmail.com>
2015-06-06 00:12:02 +0900

    セットアップスクリプト

---
+++ setup.sh
@@ -0,0 +1,6 @@
+ln -s ~/dotfiles/.gitignore_global ~/.gitignore_global
+ln -s ~/dotfiles/.gvimrc  ~/.gvimrc
+ln -s ~/dotfiles/.tmux.conf ~/.tmux.conf
+ln -s ~/dotfiles/.vim ~/.vim
+ln -s ~/dotfiles/.vimrc ~/.vimrc
+ln -s ~/dotfiles/.zshrc ~/.zshrc
